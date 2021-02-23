d8f22b14224cfe70e0fb947af36f9d5287a2bdb5
alpageush <71350409+alpageush@users.noreply.github.com>
2021-02-23 10:28:20 +0100

    Add files via upload

--- zadanie 1.9/zadanie.java
+++ zadanie 1.9/zadanie.java
@@ -1,7 +1,17 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class zadanie {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner nadzieja = new Scanner(System.in);
+        Random rand = new Random();
+        //int[] glowna_tablica = {rand.nextInt(1, 6)};
+        String[] siemandero = {"1", "2", "3"};
+        System.out.println(siemandero[1]);
+        //int siema = 
+        //for(int i=0; i<=10; i++) glowna_tablica += 
+
+        System.out.println("zyje");
+
     }
 }
