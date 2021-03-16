61957bc83f81752b5d5648630445cc10802bdf92
alpageush <71350409+alpageush@users.noreply.github.com>
2021-03-16 10:06:01 +0100

    Add files via upload

---
+++ zadania 16.03/DaneOsobowe.java
@@ -0,0 +1,6 @@
+class DaneOsobowe{
+    private String pesel = "12345678910";
+    public int wiek = 20;
+    public int wzrost = 177;
+    public int waga = 88;
+}
---
+++ zadania 16.03/PolaPrywatnePubliczne.java
@@ -0,0 +1,8 @@
+class PolaPrywatnePubliczne{
+    static DaneOsobowe dos;
+    
+    public static void main(String [] args){
+        dos = new DaneOsobowe();
+        System.out.println(dos.pesel);
+    }
+}
